b28b6953878f470a307f4bddb1227dfc4fcc4efb
kwaegel <kwaegel@acm.org>
2010-09-09 02:04:28 -0400

    Added some comments the abstract getIntersection method.

--- RayTracingEngine/SceneStructures/Scene.cs
+++ RayTracingEngine/SceneStructures/Scene.cs
@@ -34,6 +34,14 @@ namespace Raytracing.SceneStructures
 
 		public abstract void add(PointLight light);
 
+		/// <summary>
+		/// Returns the distance nearest object a ray collides with. Returns -1 if no object was hit.
+		/// </summary>
+		/// <param name="r"></param>
+		/// <param name="collisionPoint"></param>
+		/// <param name="surfaceNormal"></param>
+		/// <param name="material"></param>
+		/// <returns>The distance to the nearest object a ray collided with.</returns>
 		public abstract float getNearestIntersection(ref Ray r, ref Vector3 collisionPoint,
 			ref Vector3 surfaceNormal, ref Material material);
 
