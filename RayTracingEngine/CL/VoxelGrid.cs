a25a37dd5b9c43239dcfaac8ae9ae9508869bbad
kwaegel <kwaegel@acm.org>
2010-10-09 23:07:12 -0400

    Updated voxel grid test cells to outline the corners of the grid.

--- RayTracingEngine/CL/VoxelGrid.cs
+++ RayTracingEngine/CL/VoxelGrid.cs
@@ -10,7 +10,7 @@ namespace Raytracing.CL
 	{
 		internal ComputeImage3D _voxelGrid;
 		private float _gridWidth;
-		private int _gridResolution;
+		public int GridResolution { get; private set; }
 		public float CellSize
 		{
 			get;
@@ -20,13 +20,28 @@ namespace Raytracing.CL
 		public VoxelGrid(ComputeCommandQueue commandQueue, float gridWidth, int gridResolution)
 		{
 			_gridWidth = gridWidth;
-			_gridResolution = gridResolution;
+			GridResolution = gridResolution;
 			CellSize = gridWidth / gridResolution;
 
 			// create test data. gridResolution^3 cells
 			int cellCount = gridResolution* gridResolution* gridResolution;
 			Color4[,,] colorArray = new Color4[gridResolution, gridResolution, gridResolution];
 
+            int gridMax = gridResolution - 1;
+
+            colorArray[0, 0, 0] = Color4.Black;
+
+            colorArray[gridMax, 0, 0] = Color4.Red;
+            colorArray[0, gridMax, 0] = Color4.Green;
+            colorArray[0, 0, gridMax] = Color4.Blue;
+
+            colorArray[gridMax, gridMax, 0] = Color4.Yellow;
+            colorArray[gridMax, 0, gridMax] = Color4.Magenta;
+            colorArray[0, gridMax, gridMax] = Color4.Cyan;
+
+            colorArray[gridMax, gridMax, gridMax] = Color4.White;
+
+            /*
 			colorArray[4, 4, 4] = Color4.Red;
 			colorArray[4, 4, 5] = Color4.Green;
 			colorArray[4, 5, 4] = Color4.Blue;
@@ -35,6 +50,7 @@ namespace Raytracing.CL
 			colorArray[5, 4, 5] = Color4.DarkGreen;
 			colorArray[5, 5, 4] = Color4.DarkBlue;
 			colorArray[5, 5, 5] = Color4.DarkGoldenrod;
+             * */
 
 			ComputeImageFormat cif = new ComputeImageFormat(ComputeImageChannelOrder.Rgba, ComputeImageChannelType.Float);
 
