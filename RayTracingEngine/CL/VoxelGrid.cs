4e58841a2788c9d32b26f169c1334fea98850bd4
kwaegel <kwaegel@acm.org>
2010-10-24 02:42:06 -0400

    Made the voxelGrid class more general.

--- RayTracingEngine/CL/VoxelGrid.cs
+++ RayTracingEngine/CL/VoxelGrid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+using OpenTK;
 using OpenTK.Graphics;
 
 using Cloo;
@@ -8,40 +9,55 @@ namespace Raytracing.CL
 {
 	class VoxelGrid
 	{
+		// Compute queue
+		ComputeCommandQueue _commandQueue;
+
+		// Grid buffer.
+		private Color4[] _voxelArray;
 		internal ComputeImage3D _voxelGrid;
+
+		// Grid description.
 		private float _gridWidth;
 		public int GridResolution { get; private set; }
-		
+
+		private Vector3 _gridOrigin;
+
 		public float CellSize
 		{
 			get;
 			private set;
 		}
 
-		private Color4[] _colorArray;
+		
 		public Color4 this[int x, int y, int z]
 		{
 			get
 			{
 				int index = z * GridResolution * GridResolution + y * GridResolution + x;
-				return _colorArray[index];
+				return _voxelArray[index];
 			}
 			private set
 			{
 				int index = z * GridResolution * GridResolution + y * GridResolution + x;
-				_colorArray[index] = value;
+				_voxelArray[index] = value;
 			}
 		}
 
 		public VoxelGrid(ComputeCommandQueue commandQueue, float gridWidth, int gridResolution)
 		{
+			_commandQueue = commandQueue;
+
 			_gridWidth = gridWidth;
 			GridResolution = gridResolution;
 			CellSize = gridWidth / gridResolution;
 
+
+			Vector3 halfGridWidth = new Vector3(gridWidth/2.0f, gridWidth/2.0f, gridWidth/2.0f);
+			_gridOrigin = -halfGridWidth;
+
 			// create test data. gridResolution^3 cells
 			int cellCount = gridResolution * gridResolution * gridResolution;
-			_colorArray = new Color4[cellCount];
+			_voxelArray = new Color4[cellCount];
 
 			int gridMax = gridResolution - 1;
 
@@ -77,9 +93,9 @@ namespace Raytracing.CL
 
 			unsafe
 			{
-				fixed (Color4* gridData = _colorArray)
+				fixed (Color4* gridData = _voxelArray)
 				{
-					_voxelGrid = new ComputeImage3D(commandQueue.Context,
+					_voxelGrid = new ComputeImage3D(_commandQueue.Context,
 						ComputeMemoryFlags.CopyHostPointer | ComputeMemoryFlags.ReadOnly,
 						cif,
 						gridResolution, gridResolution, gridResolution,
@@ -92,6 +108,51 @@ namespace Raytracing.CL
 		public void Dispose()
 		{
 			_voxelGrid.Dispose();
+		}
+
+
+		public void addSphere(Vector3 center, float radius, Color4 color)
+		{
+			// Translate to grid space.
+			Vector3 gridCenter = center - _gridOrigin;
+
+			// Find center voxel.
+			int x = (int)(gridCenter.X / CellSize);
+			int y = (int)(gridCenter.Y / CellSize);
+			int z = (int)(gridCenter.Z / CellSize);
+
+			// Only color the center voxel for now.
+			this[x, y, z] = color;
+		}
+
+		public void syncBuffers()
+		{
+			
+		}
+
+		private void clampToGrid(ref int x, ref int y, ref int z)
+		{
+			// check lower bounds
+			if (x < 0)
+				x = 0;
+			if (y < 0)
+				y = 0;
+			if (z < 0)
+				z = 0;
+
+			// check upper bounds
+			if (x >= GridResolution)
+			{
+				x = GridResolution - 1;
+			}
+			if (y >= GridResolution)
+			{
+				y = GridResolution - 1;
+			}
+			if (z >= GridResolution)
+			{
+				z = GridResolution - 1;
+			}
 		}
 
 	}
