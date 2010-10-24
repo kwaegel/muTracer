bb183356a3e8a9ee17b50d4bc58f354c56f2d0bc
kwaegel <kwaegel@acm.org>
2010-10-24 02:40:11 -0400

    Re-added file that should have not been removed.

---
+++ RayTracingEngine/AxisAlignedBoundingBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenTK;
+
+namespace Raytracing.BoundingVolumes
+{
+	public class AxisAlignedBoundingBox
+	{
+		public Vector3 Min;
+		public Vector3 Max;
+
+		public AxisAlignedBoundingBox(Vector3 min, Vector3 max)
+		{
+			Min = min;
+			Max = max;
+		}
+
+
+	}
+}
